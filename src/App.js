37fc95cc9bdf257a56be730829e156a9c4683437
rotemshaked <rotemshaked8@gmail.com>
2022-03-02 11:51:56 +0200

    change player

--- src/App.js
+++ src/App.js
@@ -6,29 +6,100 @@ import { useState } from "react";
 
 function App() {
   const [diceNumber, setDiceNumber] = useState(1);
+  const [scorePlayer1, setScorePlayer1] = useState(0);
+  const [scorePlayer2, setScorePlayer2] = useState(0);
+  const [currentPlayer1Score, setCurrentPlayer1Score] = useState(0);
+  const [currentPlayer2Score, setCurrentPlayer2Score] = useState(0);
+  const [diceExist, setDiceExist] = useState(false);
+  const [player1Turn, setPlayer1Turn] = useState(true);
+  const [winner, setWinner] = useState(false);
 
-  const handleRollDiceClick = () => {
-    let randomNumber = Math.floor(Math.random() * 6) + 1;
-    setDiceNumber(randomNumber);
-    console.log(randomNumber);
+  const handleRollDice = () => {
+    if (!winner) {
+      let randomNumber = Math.floor(Math.random() * 6) + 1;
+      setDiceNumber(randomNumber);
+      setDiceExist(true);
+      if (player1Turn) {
+        setCurrentPlayer1Score((randomNumber += currentPlayer1Score));
+      } else {
+        setCurrentPlayer2Score((randomNumber += currentPlayer2Score));
+      }
+    }
   };
+
+  const handleHoldClick = () => {
+    if (player1Turn) {
+      let current = scorePlayer1 + currentPlayer1Score;
+      setScorePlayer1(current);
+      setPlayer1Turn(!player1Turn);
+      setCurrentPlayer1Score(0);
+      if (current > 30) {
+        setDiceExist(false);
+        setWinner(true);
+      }
+    } else {
+      let current = scorePlayer2 + currentPlayer2Score;
+      setScorePlayer2(current);
+      setPlayer1Turn(!player1Turn);
+      setCurrentPlayer2Score(0);
+      if (current > 30) {
+        setDiceExist(false);
+        setWinner(true);
+      }
+    }
+  };
+
+  const newGame = () => {
+    setCurrentPlayer1Score(0);
+    setCurrentPlayer2Score(0);
+    setScorePlayer1(0);
+    setScorePlayer2(0);
+    setDiceExist(false);
+    setPlayer1Turn(true);
+    setWinner(false);
+  };
+
+  // const startGame = () => {
+  //   if(){
+  //     handleRollDice();
+  //     handleHoldClick()
+  //   }
+  // };
 
   return (
     <div className="game-screen">
       <div className="buttons">
-        <Button className="btn new-game" btnName=" NEW GAME"></Button>
-        <Dice diceNumber={diceNumber} />
+        <Button
+          className="btn new-game"
+          btnName=" NEW GAME"
+          onClick={newGame}
+        ></Button>
+        {diceExist && <Dice diceNumber={diceNumber} />}
         <div className="bottom-btns">
           <Button
             className="btn roll-dice"
             btnName="🎲 ROLL DICE"
-            onClick={handleRollDiceClick}
+            onClick={handleRollDice}
           ></Button>
-          <Button className="btn hold" btnName="➕ HOLD"></Button>
+          <Button
+            className="btn hold"
+            btnName="➕ HOLD"
+            onClick={handleHoldClick}
+          ></Button>
         </div>
       </div>
-      <Player playerSide="left-player" playerName="PLAYER 1" />
-      <Player playerSide="right-player" playerName="PLAYER 2" />
+      <Player
+        playerSide="left-player"
+        playerName="PLAYER 1"
+        score={scorePlayer1}
+        roll={currentPlayer1Score}
+      />
+      <Player
+        playerSide="right-player"
+        playerName="PLAYER 2"
+        score={scorePlayer2}
+        roll={currentPlayer2Score}
+      />
     </div>
   );
 }
--- src/components/player.jsx
+++ src/components/player.jsx
@@ -1,14 +1,12 @@
 import "../assets/styles.css";
 
-const Player = ({ playerSide, playerName }) => {
-  let score = "0";
-  let roll = 6;
+const Player = ({ playerSide, playerName, score, roll }) => {
   return (
     <div className={`player-board ${playerSide}`}>
       <div className="player-name">{playerName}</div>
       <div className="score">{score}</div>
       <div className="current-roll">
-        <div className="current">CURRENT</div>
+        <div className="current">CURRENT ROLL</div>
         <div className="roll">{roll}</div>
       </div>
     </div>
